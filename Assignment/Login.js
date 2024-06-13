e498d98b6e3b15908645692a6f569e105c9ef6cc
calebtetteh2000 <calebtetteh65@gmail.com>
2024-06-13 00:23:51 +0000

    First Commit

---
+++ Assignment/Login.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Image,  } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+export default function App(){
+    return(
+        <View style={style.container}>
+            <Text style={style.headName}></Text>
+        </View>
+    );
+}
+
+const style = StyleSheet.create({
+
+})
